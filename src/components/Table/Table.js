4758518b6701b2137c8adfc0266a06aa78c32963
jir999 <jir999@users.noreply.github.com>
2020-11-08 13:19:26 +0400

    commit

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../Header";
 // import Column from "../Column";
 import Row from "../Row";
 import Footer from "../Footer";
 
 const Table = () => {
+  const [data, setData] = useState([]);
+  let fakeData;
+
+  useEffect(() => {
+    let newArr = [];
+    fetch("./data.json")
+      .then((res) => {
+        //console.log(res)
+        return res.json()
+      })
+      .then((arr) => {
+        console.log("result", arr)
+        fakeData = arr;
+        setData(arr)
+      });
+  }, []);
+
+  useEffect(() => console.log("fakeData", fakeData))
+  useEffect(() => console.log("state", data))
+
   return (
     <div>
       <table>
         <Header />
         {/* <Column /> */}
-        <Row />
+        <Row rowData={fakeData} />
         <Footer />
       </table>
     </div>
