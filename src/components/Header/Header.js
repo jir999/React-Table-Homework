714fc62eaa013b59a461598fca536e0832975c59
jir999 <jir999@users.noreply.github.com>
2020-11-13 14:40:41 +0400

    fixing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,10 +6,7 @@ const Header = ({ headerData }) => {
   return (
     <thead>
       <tr>
-        <th>{headerData[0]}</th>
-        <th>{headerData[1]}</th>
-        <th>{headerData[2]}</th>
-        <th>{headerData[3]}</th>
+        {headerData.map((item) => <th>{item}</th>)}
       </tr>
     </thead>
   );
